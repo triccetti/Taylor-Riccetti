a9d68448e1b301762d0ea2a09bba1cd9c4de34e0
triccetti <t.riccetti4@live.com>
2018-10-27 22:21:14 -0700

    redesign!

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-#js file
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+window.onload = function() { 
+};
